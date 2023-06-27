f87e1002d06c678781fcdcc9d8c3b99c18cd837a
Erzn3522 <abdullah_25145@hotmail.com>
2023-06-27 22:40:54 +0300

    deleted

--- windowCreator.py
+++
@@ -1,7 +0,0 @@
-import cv2 
-#Abdullah Erzin
-#this is a void func which is creating a display with a name with the upper and lower threshold limits
-def WindowCeator(lowerLimit, upperLimit, img):
-    windowName =f'threshold between {lowerLimit} and {upperLimit}'
-    cv2.namedWindow(windowName)    
-    cv2.imshow(windowName, img)
