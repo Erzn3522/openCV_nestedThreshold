f8ff6f0fd38ea2b72d8f334a617ac80593980ff5
Erzn3522 <abdullah_25145@hotmail.com>
2023-06-27 22:41:33 +0300

    function usage changed

--- threholdTest.py
+++ threholdTest.py
@@ -1,16 +1,20 @@
 import cv2
-from windowCreator import *
 from threshold_intersection import *
 
 imgBase = cv2.imread('images/transition2.png')
 grayImage = cv2.cvtColor(imgBase, cv2.COLOR_BGR2GRAY)
-#describing the up and low limit for the threshold function
-lowerVal = 0
-upperVal = 100
+# describing the up and low limit for the threshold function
+lowerLimit = -1
+upperLimit = 100
+windowName =f'threshold between {lowerLimit} and {upperLimit}'
 
 #this func creating a function according to out parameters
-# Check the windowCreator.py and  threshold_Intersection.pyfile for more details 
-WindowCeator( lowerVal, upperVal, thresholdIntersection(imgBase, grayImage, lowerVal, upperVal))
+nestedImage , contours = thresholdIntersection(imgBase, grayImage, lowerLimit, upperLimit)
 
+# Draw the contours on the original image
+cv2.drawContours(nestedImage, contours, -1, (0, 0, 255), 2)
+
+cv2.namedWindow(windowName)    
+cv2.imshow(windowName, nestedImage)
 cv2.waitKey(0) 
 cv2.destroyAllWindows()
